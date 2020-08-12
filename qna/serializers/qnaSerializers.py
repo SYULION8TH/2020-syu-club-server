9769206e18197c4655ce5a950f647483d89fcac1
jisoolee11 <jisu6414@likelion.org>
2020-08-12 17:12:55 +0900

    error fixed

--- qna/serializers/qnaSerializers.py
+++ qna/serializers/qnaSerializers.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD:qna/serializers/qna_serializers.py
-=======
 from user.models import ClubsQna
 from rest_framework import serializers
 
--- qna/urls.py
+++ qna/urls.py
@@ -1,11 +1,11 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import qnaViews
 
-# router = DefaultRouter()
-# router.register('qna', views.QnaViewSet)
+router = DefaultRouter()
+router.register('qna', qnaViews.QnaViewSet)
 # router.register('qna_comment', views.Qna_CommentViewSet)
 
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
+urlpatterns = [
+    path('', include(router.urls)),
+]
