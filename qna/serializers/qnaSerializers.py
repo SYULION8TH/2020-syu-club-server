f79e17ef5b9701b965d9f19d5a8f2f3ef730c720
jisoolee11 <jisu6414@likelion.org>
2020-08-16 12:11:28 +0900

    qnareplies

--- qna/serializers/qnaSerializers.py
+++ qna/serializers/qnaSerializers.py
@@ -1,7 +1,10 @@
 from user.models import ClubsQna
 from rest_framework import serializers
+from qna.serializers import qnaCommentSerializers
 
 class QnaSerializer(serializers.ModelSerializer):
+    qnareplies = qnaCommentSerializers.QnaRepliesSerializer(many=True, read_only=True)
+
     class Meta:
         model = ClubsQna
         fields = '__all__'
--- qna/urls.py
+++ qna/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import qnaViews
+from .views import qnaviews
+from .views import qnaCommentViews
 
 router = DefaultRouter()
-router.register('qna', qnaViews.QnaViewSet)
-# router.register('qna_comment', views.Qna_CommentViewSet)
+router.register('qna', qnaviews.QnaViewSet)
+router.register('qnareplies', qnaCommentViews.QnaRepliesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
