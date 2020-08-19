bab1b74fce4ae22f0c18daef2670866463f1a3b4
jisoolee11 <jisu6414@likelion.org>
2020-08-19 14:21:55 +0900

    secretreplies

--- qna/serializers/qnaSerializers.py
+++ qna/serializers/qnaSerializers.py
@@ -1,6 +1,6 @@
 from user.models import ClubsQna
 from rest_framework import serializers
-from qna.serializers import qnaCommentSerializers
+from qna.serializers import qnaRepliesSerializers
 
 class QnaSerializer(serializers.ModelSerializer):
     # qnareplies = qnaCommentSerializers.QnaRepliesSerializer(many=True)
