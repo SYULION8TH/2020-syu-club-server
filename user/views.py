3955bc8bb616c153e0c9c67f59708d477ca81321
sneakstarberry <gihong2012@likelion.org>
2020-08-21 18:39:44 +0900

    anonymousUser added

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, status
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from rest_framework.response import Response
+from django.contrib.auth.models import AnonymousUser
 import json
 
 class InfoGenerics(APIView):
